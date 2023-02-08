725b7265e0dc24b57c4c4863d73fb36e397d32b8
kevelscy <kevelscy@gmail.com>
2023-02-08 14:45:55 -0400

    using environment variables to API PATHS

--- next.config.js
+++ next.config.js
@@ -9,7 +9,7 @@ module.exports = {
     return [
       {
         source: '/api/:path*',
-        destination: 'https://to-change/api/:path*'
+        destination: `${process.env.API_URL}/api/:path*`
       },
       {
         source: '/recursos',
