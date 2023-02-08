60388cea5aaaf91672c6661d45b5fa1f1d137b12
kevelscy <kevelscy@gmail.com>
2023-02-08 14:29:15 -0400

    test rewrite dynamic api path

--- next.config.js
+++ next.config.js
@@ -9,7 +9,7 @@ module.exports = {
     return [
       {
         source: '/api/:path*',
-        destination: `${process.env.API_URL}/api/:path*`
+        destination: 'to_change/api/:path*'
       },
       {
         source: '/recursos',
