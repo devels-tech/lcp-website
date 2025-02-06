c608ee9d17c338b2f76dd4f3490edcc014a8a115
kevelscy <kevelscy@gmail.com>
2025-02-06 06:11:21 -0400

    close evnets

--- src/pages/eventos/index.tsx
+++ src/pages/eventos/index.tsx
@@ -43,10 +43,10 @@ export const EventosPage: Page = () => {
 
       <div className='min-h-screen md:min-h-0 w-full pb-4 px-4'>
         <div className='w-full h-full flex flex-col justify-center items-center pt-32 md:pt-32'>
-          {/* <EmptyEvents /> */}
+          <EmptyEvents />
 
-          <span className='text-4xl md:text-7xl font-black text-gray-900'>Eventos</span>
-          <br />
+          {/* <span className='text-4xl md:text-7xl font-black text-gray-900'>Eventos</span>
+          <br /> */}
 
           <ul className='flex flex-wrap justify-center items-center max-w-5xl gap-6'>
             {/* <li>
@@ -67,14 +67,14 @@ export const EventosPage: Page = () => {
               />
             </li>  */}
 
-            <li>
+            {/* <li>
               <Event
                 localImage={edcImgSrc}
                 title='Inscripciones EDC'
                 redirectTo='/eventos/edc'
                 redirectLabel='Inscripciones EDC'
               />
-            </li>
+            </li> */}
 
             {/* <li>
               <Event
@@ -96,50 +96,6 @@ export const EventosPage: Page = () => {
               />
             </li> */}
           </ul>
-
-          {/* <Link href='/'>
-            <a className='text-lg text-secondary-500 xl:text-xl underline underline-offset-1'>Ir al inicio</a>
-          </Link> */}
-          {/* <span className='text-3xl md:text-5xl font-black text-gray-900'>Eventos</span>
-          <br /> */}
-
-          {/* <ul className='flex flex-wrap justify-center items-center max-w-5xl mx-auto gap-x-2'> */}
-
-          {/* </ul> */}
-
-          {/* {
-            events.length !== 0
-              ? <>
-                <span className='text-3xl md:text-5xl font-black text-gray-900'>Eventos</span>
-                <br />
-
-                <ul className='flex flex-wrap justify-center items-center max-w-5xl gap-x-2'>
-                  {
-                    events.map(event => (
-                      <li key={event.id}>
-                        <Event
-                          key={event.title}
-                          imgSrc={event.imgSrc}
-                          title={event.title}
-                          description={event.description}
-                          redirectTo={event.redirectTo}
-                          redirectLabel={event.redirectLabel}
-                          date={event.date}
-                          schedule={event.schedule}
-                        />
-                      </li>
-                    ))
-                  }
-                </ul>
-              </>
-              : <div className='pt-20 text-center'>
-                <span className='text-4xl md:text-7xl font-black text-gray-900'>Sin Eventos</span>
-                <br />
-                <Link href='/'>
-                  <a className='text-lg text-secondary-500 xl:text-xl underline underline-offset-1'>Ir al inicio</a>
-                </Link>
-              </div>
-          } */}
         </div>
       </div>
     </>
